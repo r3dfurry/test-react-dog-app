83af4e091804dec49bfa898f94de5a93637a742a
Nikita Dyrdin <Nikita_Dyrdin@epam.com>
2018-07-30 16:56:46 +0300

    Implemented basic routing

---
+++ src/common/not-found.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class NotFound extends React.Component<any, any> {
+    public render() {
+        return(
+            <p>Route not found!</p>
+        )
+    }
+}
+
+export default NotFound;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,26 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import App from './features/dog-show/components/main/main';
+import NotFound from './common/not-found';
 import configureStore from './features/dog-show/store';
 import './index.css';
 import 'semantic-ui/dist/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 
 const store = configureStore();
 
 render(
-  <App store={store} />,
+  <Router>
+      <Switch>
+        <Route path="/dogs">
+          <App store={store} />
+        </Route>
+        <Route Path="*">
+          <NotFound />      
+        </Route>
+      </Switch>
+  </Router>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
